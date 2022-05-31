e1e761680fe0d4165aea24ba463d4a1b2b0e4daf
ajlekcahdp4 <alex.rom23@mail.ru>
2022-05-31 14:08:30 +0300

    abstracting triangle with templates

---
+++ 01-basics/triangle-common.cc
@@ -0,0 +1,30 @@
+//-----------------------------------------------------------------------------
+//
+// Triangles demo: abstracting with templates
+//
+//-----------------------------------------------------------------------------
+
+#include <iostream>
+
+template <typename T> struct Point {T x,y;};
+
+template <typename U> struct Triangle 
+{
+    Point<U> pts[3];
+    U double_square ()
+    {
+        U sq =  pts[0].x * (pts[1].y + pts[2].y) + 
+                pts[1].x * (pts[0].y + pts[2].y) +
+                pts[2].x * (pts[0].y + pts[1].y);
+        return (sq > 0) ? sq : -sq;
+    }
+};
+
+int main() {
+  struct Triangle<double> t;
+  t.pts[0] = {1.0, 1.0};
+  t.pts[1] = {3.0, 3.0};
+  t.pts[2] = {1.0, 2.0};
+  double square = t.double_square() / 2.0;
+  std::cout << square << std::endl;
+}
