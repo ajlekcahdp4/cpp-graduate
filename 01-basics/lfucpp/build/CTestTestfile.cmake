54a7388f9ecdcf1c3e568e9fc504a789622c51d0
ajlekcahdp4 <alex.rom23@mail.ru>
2022-07-15 19:33:00 +0300

    delete build folder

--- 01-basics/lfucpp/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alex/cpp/cpp-graduate/01-basics/lfucpp
-# Build directory: /home/alex/cpp/cpp-graduate/01-basics/lfucpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[TestLFU.testEndToEnd]=] "/usr/bin/bash" "/home/alex/cpp/cpp-graduate/01-basics/lfucpp/test.sh" "/home/alex/cpp/cpp-graduate/01-basics/lfucpp/build/lfucpp" "/home/alex/cpp/cpp-graduate/01-basics/lfucpp")
-set_tests_properties([=[TestLFU.testEndToEnd]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/alex/cpp/cpp-graduate/01-basics/lfucpp/CMakeLists.txt;21;add_test;/home/alex/cpp/cpp-graduate/01-basics/lfucpp/CMakeLists.txt;0;")
